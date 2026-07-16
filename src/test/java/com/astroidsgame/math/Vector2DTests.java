d5f57bb066fd256ed30a451b3ba5d6be9b35744d
JamieScofield <107922081+JamieScofield@users.noreply.github.com>
2026-07-16 21:52:43 +0100

    added tests to vec 2d

--- src/test/java/com/astroidsgame/math/Vector2DTests.java
+++ src/test/java/com/astroidsgame/math/Vector2DTests.java
@@ -16,4 +16,131 @@ public class Vector2DTests {
         assertEquals(new Vector2D(2,2), result);
     }
 
+    @Test
+    void testSubtract() {
+        var vec1 = new Vector2D(5,5);
+        var vec2 = new Vector2D(3,2);
+
+        var result = vec1.subtract(vec2);
+
+        assertEquals(new Vector2D(2, 3), result);
+    }
+
+    @Test
+    void testLength() {
+        var vec = new Vector2D(3, 4);
+
+        assertEquals(5.0, vec.length(), 1e-9);
+    }
+
+    @Test
+    void testLengthOfZeroVector() {
+        assertEquals(0.0, Vector2D.ZERO.length(), 1e-9);
+    }
+
+    @Test
+    void testScale() {
+        var vec = new Vector2D(2, 3);
+
+        var result = vec.scale(2);
+
+        assertEquals(new Vector2D(4, 6), result);
+    }
+
+    @Test
+    void testScaleByZero() {
+        var vec = new Vector2D(2, 3);
+
+        var result = vec.scale(0);
+
+        assertEquals(Vector2D.ZERO, result);
+    }
+
+    @Test
+    void testNormalize() {
+        var vec = new Vector2D(3, 4);
+
+        var result = vec.normalize();
+
+        assertEquals(0.6, result.x(), 1e-9);
+        assertEquals(0.8, result.y(), 1e-9);
+    }
+
+    @Test
+    void testNormalizeReturnsUnitLength() {
+        var vec = new Vector2D(7, -2);
+
+        var result = vec.normalize();
+
+        assertEquals(1.0, result.length(), 1e-9);
+    }
+
+    @Test
+    void testNormalizeOfZeroVectorReturnsZero() {
+        var result = Vector2D.ZERO.normalize();
+
+        assertEquals(Vector2D.ZERO, result);
+    }
+
+    @Test
+    void testLerpAtStartReturnsOrigin() {
+        var start = new Vector2D(1, 1);
+        var end = new Vector2D(9, 9);
+
+        var result = start.lerp(end, 0);
+
+        assertEquals(start, result);
+    }
+
+    @Test
+    void testLerpAtEndReturnsTarget() {
+        var start = new Vector2D(1, 1);
+        var end = new Vector2D(9, 9);
+
+        var result = start.lerp(end, 1);
+
+        assertEquals(end, result);
+    }
+
+    @Test
+    void testLerpAtMidpoint() {
+        var start = new Vector2D(0, 0);
+        var end = new Vector2D(10, 10);
+
+        var result = start.lerp(end, 0.5);
+
+        assertEquals(new Vector2D(5, 5), result);
+    }
+
+    @Test
+    void testAngleBetweenPerpendicularVectors() {
+        var vec1 = new Vector2D(1, 0);
+        var vec2 = new Vector2D(0, 1);
+
+        assertEquals(90.0, vec1.angle(vec2), 1e-9);
+    }
+
+    @Test
+    void testAngleBetweenParallelVectors() {
+        var vec1 = new Vector2D(1, 0);
+        var vec2 = new Vector2D(2, 0);
+
+        assertEquals(0.0, vec1.angle(vec2), 1e-9);
+    }
+
+    @Test
+    void testAngleBetweenOppositeVectors() {
+        var vec1 = new Vector2D(1, 0);
+        var vec2 = new Vector2D(-1, 0);
+
+        assertEquals(180.0, vec1.angle(vec2), 1e-9);
+    }
+
+    @Test
+    void testAngleWithZeroVectorIsZero() {
+        var vec = new Vector2D(1, 0);
+
+        assertEquals(0.0, vec.angle(Vector2D.ZERO), 1e-9);
+    }
+
 }
